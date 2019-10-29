463d0d4ae30eae101ae3be855d2f0ec455119ce0
user user <user@workspacet3lriwauuukrbtej.tools-555d8bc78b-njgbv>
2019-10-29 14:45:38 +0000

    commit from OpenShift Eclipse Che

--- src/main/java/ru/cb/app/domain/Person.java
+++ src/main/java/ru/cb/app/domain/Person.java
@@ -7,104 +7,37 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Person {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
 
-	@Column(name = "last_name")
-	private String lastName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(name = "first_name")
-	private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
 
-	@Column(name = "work_phone")
-	private String workPhone;
+    @Column(name = "first_name")
+    private String firstName;
 
-	@Column(name = "mobile_phone")
-	private String mobilePhone;
+    @Column(name = "work_phone")
+    private String workPhone;
 
-	@Column(name = "email")
-	private String email;
+    @Column(name = "mobile_phone")
+    private String mobilePhone;
 
-	@Column(name = "birth_date")
-	//@Convert(converter = BirthDateConverter.class)
-	@DateTimeFormat(pattern = "dd.MM.yyyy")
-	private String birthDate;
-	
-	private String company;
+    @Column(name = "email")
+    private String email;
 
-	public Long getId() {
-		return id;
-	}
+    @Column(name = "birth_date")
+    // @Convert(converter = BirthDateConverter.class)
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
+    private String birthDate;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getWorkPhone() {
-		return workPhone;
-	}
-
-	public void setWorkPhone(String workPhone) {
-		this.workPhone = workPhone;
-	}
-
-	public String getMobilePhone() {
-		return mobilePhone;
-	}
-
-	public void setMobilePhone(String mobilePhone) {
-		this.mobilePhone = mobilePhone;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(String birthDate) {
-		this.birthDate = birthDate;
-	}
-	
-	public String getCompany() {
-		return company;
-	}
-
-	public void setCompany(String company) {
-		this.company = company;
-	}
-
-	@Override
-	public String toString() {
-		return String.format(
-				"Person [id=%s, lastName=%s, firstName=%s, workPhone=%s, mobilePhone=%s, email=%s, birthDate=%s]",
-				id, lastName, firstName, workPhone, mobilePhone, email, birthDate);
-	}
+    @Column(name = "company")
+    private String company;
 
 }
--- src/main/java/ru/cb/app/domain/Work.java
+++ src/main/java/ru/cb/app/domain/Work.java
@@ -8,75 +8,32 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Work {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(name = "last_name")
-	@NotBlank(message = "must be not blank!")
-	@Size(max = 20)
-	private String lastName;
+    @Column(name = "last_name")
+    @NotBlank(message = "must be not blank!")
+    @Size(max = 20)
+    private String lastName;
 
-	@Column(name = "first_name")
-	@NotBlank
-	@Size(max = 10)
-	private String firstName;
+    @Column(name = "first_name")
+    @NotBlank
+    @Size(max = 10)
+    private String firstName;
 
-	@NotBlank
-	private String company;
+    @NotBlank
+    @Column(name = "company")
+    private String company;
 
-	@NotBlank
-	private String address;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getCompany() {
-		return company;
-	}
-
-	public void setCompany(String company) {
-		this.company = company;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("Work [id=%s, lastName=%s, firstName=%s, company=%s, address=%s]", id, lastName, firstName,
-				company, address);
-	}
-	
-	
+    @NotBlank
+    @Column(name = "address")
+    private String address;
 
 }
